8ca9be843a1a478704ed18a16e4674debd2322f1
ja2003aa <127331382+ja2003aa@users.noreply.github.com>
2025-12-01 04:37:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     var options = {
-        strings: ["Machine Learning Engineer", "Developer", "Web Designer", "Data Scientits", "Script Writer"],
+        strings: ["Machine Learning Engineer", "AI Developer", "Web Designer", "Data Scientits", "Script Writer"],
         typeSpeed: 50,
         backSpeed: 25,
         backDelay: 2000,
@@ -10,3 +10,4 @@ document.addEventListener('DOMContentLoaded', function () {
 
     var typed = new Typed("#typed-text", options);
 });
+
